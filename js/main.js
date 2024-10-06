ebc012ff559aa9e470692f9c2821ee2218170b14
evans <evansmunatsa7@gmail.com>
2024-10-06 13:55:13 +0200

    more edits

--- js/main.js
+++ js/main.js
@@ -74,6 +74,21 @@
         ]
     });
 
+
+          // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
     
 })(jQuery);
 
